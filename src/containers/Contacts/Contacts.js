2d4365ec36937dff94d40ea896dae28f0b19ed82
ваше имя < >
2019-02-28 00:05:13 +0600

    Добавлен компонент Contacts.js

---
+++ src/containers/Contacts/Contacts.js
@@ -0,0 +1,19 @@
+import React, {Component, Fragment} from 'react';
+import './Contacts.css';
+
+
+class Contacts extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h3>
+                    “We envision a world where hundreds of millions of people collaborate in teams—however they
+                    like—with their imaginations being the only constraint for what they can accomplish.”
+                </h3>
+                <p>— Michael Pryor <br/> Head of Trello</p>
+            </Fragment>
+        );
+    }
+}
+
+export default Contacts;
